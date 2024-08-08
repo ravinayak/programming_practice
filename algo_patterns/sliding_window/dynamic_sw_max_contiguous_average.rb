df376d760fb30e8df232fcf2506372cce8afdef2
Ravi Kumar Nayak <ravikumarnayak@Ravis-MacBook-Pro.local>
2024-08-08 11:46:00 -0400

    Fix indentation

--- algo_patterns/sliding_window/dynamic_sw_max_contiguous_average.rb
+++ algo_patterns/sliding_window/dynamic_sw_max_contiguous_average.rb
@@ -1,31 +1,32 @@
+# frozen_string_literal: true
+
 # Find the maximum contiguous size for elements in an array where the maximum different elements
 # in the contiguous sequence can only be k
 # This is dynamic window sliding problem
 # @param [Array] input_arr
 # @param [Integer] k
-# @return [Hash] max_sw_size_arr 
+# @return [Hash] max_sw_size_arr
 #
 # arr = [1,2,1,2,1,2,3,4,5,6,6,8,6,8,8,6,6,7]
 def dynamic_sw_max_contiguous_sequence(input_arr, k)
-	return { max_size: 0, max_sw_arr: [] } if input_arr.empty? || k == 0
+  return { max_size: 0, max_sw_arr: [] } if input_arr.empty? || k.zero?
 
-	sw_pointers = { left: 0, right: 0 }
-	max_sw_size_arr = { max_size: 0, max_sw_arr: [] }
-	k_elements_count_hsh = Hash.new(0)
+  sw_pointers = { left: 0, right: 0 }
+  max_sw_size_arr = { max_size: 0, max_sw_arr: [] }
+  k_elements_count_hsh = Hash.new(0)
 
-	while sw_pointers[:right] < input_arr.length
-		k_elements_count_hsh[input_arr[sw_pointers[:right]]] += 1
-		while k_elements_count_hsh.size > k
-			adjust_elements_count_hsh(k_elements_count_hsh, k, input_arr, sw_pointers)
-		end
-		assign_max_sw_size_arr(input_arr, sw_pointers, max_sw_size_arr) if size_greater_max_size?(sw_pointers, max_sw_size_arr)
-		sw_pointers[:right] += 1
-	end
+  while sw_pointers[:right] < input_arr.length
+    k_elements_count_hsh[input_arr[sw_pointers[:right]]] += 1
+    adjust_elements_count_hsh(k_elements_count_hsh, k, input_arr, sw_pointers) while k_elements_count_hsh.size > k
+    assign_max_sw_size_arr(input_arr, sw_pointers, max_sw_size_arr) if size_greater_max_size?(sw_pointers,
+                                                                                              max_sw_size_arr)
+    sw_pointers[:right] += 1
+  end
 
-	puts "Maxium contiguous size for elements in array where maximum different elements is #{k} :: #{max_sw_size_arr[:max_size]}"
-	puts "Elements in the contiguous sequence is :: ##{max_sw_size_arr[:max_sw_arr]}"
+  puts "Maxium contiguous size for elements in array where maximum different elements is #{k} :: #{max_sw_size_arr[:max_size]}"
+  puts "Elements in the contiguous sequence is :: ##{max_sw_size_arr[:max_sw_arr]}"
 
-	[max_sw_size_arr]
+  [max_sw_size_arr]
 end
 
 # Adjusts elements in hash based on input parameter k
@@ -35,11 +36,11 @@ end
 # @param [Hash] sw_pointers
 # @return [NIL]
 #
-def adjust_elements_count_hsh(k_elements_count_hsh, k, input_arr, sw_pointers)
-	key = input_arr[sw_pointers[:left]]
-	k_elements_count_hsh[key] -= 1
-	k_elements_count_hsh.delete(key) if k_elements_count_hsh[key] == 0
-	sw_pointers[:left] += 1
+def adjust_elements_count_hsh(k_elements_count_hsh, _k, input_arr, sw_pointers)
+  key = input_arr[sw_pointers[:left]]
+  k_elements_count_hsh[key] -= 1
+  k_elements_count_hsh.delete(key) if (k_elements_count_hsh[key]).zero?
+  sw_pointers[:left] += 1
 end
 
 # Assign max size and max sliding window elements to max array
@@ -49,8 +50,8 @@ end
 # @return [NIL]
 #
 def assign_max_sw_size_arr(input_arr, sw_pointers, max_sw_size_arr)
-	max_sw_size_arr[:max_size] = cal_sw_size(sw_pointers)
-	max_sw_size_arr[:max_sw_arr] = input_arr[sw_pointers[:left]..sw_pointers[:right]]
+  max_sw_size_arr[:max_size] = cal_sw_size(sw_pointers)
+  max_sw_size_arr[:max_sw_arr] = input_arr[sw_pointers[:left]..sw_pointers[:right]]
 end
 
 # checks if current max size is less than sliding window size
@@ -59,10 +60,10 @@ end
 # @return [Boolean]
 #
 def size_greater_max_size?(sw_pointers, max_sw_size_arr)
-	sw_size = cal_sw_size(sw_pointers)
-	return true if sw_size > max_sw_size_arr[:max_size]
+  sw_size = cal_sw_size(sw_pointers)
+  return true if sw_size > max_sw_size_arr[:max_size]
 
-	false
+  false
 end
 
 # Calculates the sliding window size
@@ -70,13 +71,13 @@ end
 # @return [Integer]
 #
 def cal_sw_size(sw_pointers)
-	sw_pointers[:right] - sw_pointers[:left] + 1
+  sw_pointers[:right] - sw_pointers[:left] + 1
 end
 
 # arr = [[1,2,3,2,2,1,2,4,2,1], [1,2,3,2,2], [1,2,-1,2,2], [1,2,3,4,5,6],[1,2,1,2,1,2,3,4,5,6,6,8,6,8,8,6,6,7]]
-arr = [[1,2,1,2,1,2,1,2,3,4,-1,-2,1,2,1,2,3,4,2,2,1,4,3,2,1,3,2,3,4,8,-9,10]]
+arr = [[1, 2, 1, 2, 1, 2, 1, 2, 3, 4, -1, -2, 1, 2, 1, 2, 3, 4, 2, 2, 1, 4, 3, 2, 1, 3, 2, 3, 4, 8, -9, 10]]
 arr.each do |input_arr|
-	puts "Input Arr :: #{input_arr}"
-	dynamic_sw_max_contiguous_sequence(input_arr, 4)
-	puts ""
+  puts "Input Arr :: #{input_arr}"
+  dynamic_sw_max_contiguous_sequence(input_arr, 4)
+  puts ''
 end
--- algo_patterns/sliding_window/fixed_sw_max_contiguous_average.rb
+++ algo_patterns/sliding_window/fixed_sw_max_contiguous_average.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Find the maximum average of contiguous 4 elements in a given array
 # youtube.com => https://www.youtube.com/watch?v=XfSgQvKfcys&list=PL7g1jYj15RUOjoeZAJsWjwV8XUo9r0hwc&index=2
 #
@@ -8,17 +10,16 @@
 # @return [Integer] max_avg
 # Time Complexity O(n*k) ~ O(n^2)
 #
-def max_contiguous_avg(input_arr, k)
-	max_avg = 0
-	return max_avg if input_arr.empty? || input_arr.length < 4
+def max_contiguous_avg(input_arr, _k)
+  max_avg = 0
+  return max_avg if input_arr.empty? || input_arr.length < 4
 
-
-	input_arr.each_with_index do |element, index|
-		avg = (element + input_arr[index+1] + input_arr[index+2] + input_arr[index+3])/4.to_f
-		max_avg = avg if max_avg < avg
-		break if (index + 4) == input_arr.length
-	end
-	max_avg
+  input_arr.each_with_index do |element, index|
+    avg = (element + input_arr[index + 1] + input_arr[index + 2] + input_arr[index + 3]) / 4.to_f
+    max_avg = avg if max_avg < avg
+    break if (index + 4) == input_arr.length
+  end
+  max_avg
 end
 
 # arr = [[1, 12, -5, -6, 50, 3], [1,2,3], [3, 4, 5, -5], [1, 100, 9, -81, 82, 67, 120]]
@@ -33,22 +34,23 @@ end
 # @return [Integer] max_avg
 # Time Complexity O(n)
 #
-def max_contiguous_avg_sw(input_arr, k)
-	max_avg = 0
-	return max_avg if input_arr.empty? || input_arr.length < 4
+def max_contiguous_avg_sw(input_arr, _k)
+  max_avg = 0
+  return max_avg if input_arr.empty? || input_arr.length < 4
 
-	start_index = 0
-	sum = (input_arr[start_index] + input_arr[start_index + 1] + input_arr[start_index + 2] + input_arr[start_index + 3])
-	input_arr.each_with_index do |element, index|
-		avg = sum/4.to_f
-		max_avg = avg if max_avg > avg
-		break if (index + 4) == input_arr.length
-		sum = (sum - input_arr[index] + input_arr[index + 4])
-	end
-	max_avg
+  start_index = 0
+  sum = (input_arr[start_index] + input_arr[start_index + 1] + input_arr[start_index + 2] + input_arr[start_index + 3])
+  input_arr.each_with_index do |_element, index|
+    avg = sum / 4.to_f
+    max_avg = avg if max_avg > avg
+    break if (index + 4) == input_arr.length
+
+    sum = (sum - input_arr[index] + input_arr[index + 4])
+  end
+  max_avg
 end
 
-arr = [[1, 12, -5, -6, 50, 3], [1,2,3], [3, 4, 5, -5], [1, 100, 9, -81, 82, 67, 120]]
+arr = [[1, 12, -5, -6, 50, 3], [1, 2, 3], [3, 4, 5, -5], [1, 100, 9, -81, 82, 67, 120]]
 arr.each do |input_arr|
-	puts "Maximum Average is :: #{max_contiguous_avg(input_arr, 4)}"
+  puts "Maximum Average is :: #{max_contiguous_avg(input_arr, 4)}"
 end
