1a27f3187043c7c40dd40b557161163ac6e91b53
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-08-17 19:14:55 -0400

    Included class methods for binary heap, increase/decrease key, insert key methods included

--- algo_patterns/heap/max_binary_heap.rb
+++ algo_patterns/heap/max_binary_heap.rb
@@ -9,144 +9,210 @@
 #
 # Class implements Binary Heap
 class MaxBinaryHeap
-  # Subtree rooted at children of node at index "i" -
-  #		left(i), right(i)
-  #	are valid max heaps, however, node at index "i" may be greater
-  # than its children - left or right and thus may violate the max heap
-  # property. This method fixes possible violation
-  # param [Array] arr
-  # @param [Integer] i
-  # @return [Array]
-  #
-  def self.max_heapify(arr:, i:, heap_size:)
-    arr_updated = prepare_arr_for_heap(arr:)
-    max_heap = max_heapify_helper(arr: arr_updated, i:, heap_size:)
-		max_heap[1..heap_size]
-  end
-
-  # Implement Heap Sort algorithm
-  # Time Complexity: O(n * log n)
-  # @param [Array] arr
-  # @param [Integer] n
-  # @return [Array]
-  #
-  def self.heapsort(arr:, n:)
-		arr_updated = prepare_arr_for_heap(arr: arr)
-		max_heap_arr = build_max_heap(arr: arr_updated, n:)
-
-		index = n
-    while index > 1
-      max_heap_arr = heapsort_helper(arr: max_heap_arr, n: index)
-      index -= 1
-    end
-    max_heap_arr[1..n]
-  end
-
-  # Build Max Heap from an array of n elements
-  # Time Complexity: O(n)
-  # @param [Array] arr
-  # @param [Integer] n
-  # @return [Array]
-  #
-  def self.build_max_heap(arr:, n:)
-    index = n / 2
-
-		while index >= 1
-      arr = max_heapify_helper(arr:, i: index, heap_size: n)
-      index -= 1
-    end
-
-    arr
-  end
-
-  # Helper method to implement max_heapify
-  # @param [Array] arr
-  # @param [Integer] i
-  # @param [Integer] heap_size
-  # @return [Array]
-  #
-  def self.max_heapify_helper(arr:, i:, heap_size:)
-    left = left(i:)
-    right = right(i:)
-
-    # index "i" is a valid index within binary heap. However, left or right
-    # child of "i" maybe outside heap size
+  class << self
+    # Subtree rooted at children of node at index "i" -
+    #		left(i), right(i)
+    #	are valid max heaps, however, node at index "i" may be greater
+    # than its children - left or right and thus may violate the max heap
+    # property. This method fixes possible violation
+    # param [Array] arr
+    # @param [Integer] i
+    # @return [Array]
     #
-    largest = if left <= heap_size && arr[left] > arr[i]
-                left
-              else
-                i
-              end
-
-    largest = right if right <= heap_size && arr[right] > arr[largest]
-
-    if largest != i
-			exchange_nodes(i:, largest:, arr:)
-			max_heapify_helper(arr:, i: largest, heap_size:) 
-		end
-
-    # return Array arr which is a valid max heap
-    arr
-  end
-
-  # Prepares Arr, heap_size and returns an array which contains
-  # those values
-  # @param [Array] arr
-  # @param [Integer] n
-  # @return [Array]
-  #
-  def self.prepare_arr_for_heap(arr:)
-    arr_updated = [nil]
-    arr.each_with_index do |element, index|
-      arr_updated[index + 1] = element
+    def max_heapify(arr:, i:, heap_size:)
+      arr_updated = prepare_arr_for_heap(arr:)
+      max_heap = max_heapify_helper(arr: arr_updated, i:, heap_size:)
+      max_heap[1..heap_size]
     end
-    arr_updated
-  end
 
-  # Helper method to implement heapsort
-  # @param [Array] arr
-  # @param [Integer] n
-  # @return [Array]
-  #
-  def self.heapsort_helper(arr:, n:)
-    exchange_nodes(i: 1, largest: n, arr:)
-    new_heap_size = n - 1
-		max_heapify_helper(arr:, i: 1, heap_size: new_heap_size)
-  end
+    # Implement Heap Sort algorithm
+    # Time Complexity: O(n * log n)
+    # @param [Array] arr
+    # @param [Integer] n
+    # @return [Array]
+    #
+    def heapsort(arr:, n:)
+      arr_updated = prepare_arr_for_heap(arr:)
+      max_heap_arr = build_max_heap(arr: arr_updated, n:)
 
-  # Exchanges nodes at index i with index_to_exchange
-  # @param [Integer] i
-  # @param [Array] arr
-  # @param [Integer] largest
-  #
-  def self.exchange_nodes(i:, largest:, arr:)
-    temp = arr[i]
-    arr[i] = arr[largest]
-    arr[largest] = temp
-  end
+      index = n
+      while index > 1
+        max_heap_arr = heapsort_helper(arr: max_heap_arr, n: index)
+        index -= 1
+      end
+      max_heap_arr[1..n]
+    end
 
-  # Left child of node at index "i" in binary heap
-  # @param [Integer] i
-  # @return [Integer]
-  #
-  def self.left(i:)
-    2 * i
-  end
+    # Build Max Heap from an array of n elements
+    # Time Complexity: O(n)
+    # @param [Array] arr
+    # @param [Integer] n
+    # @return [Array]
+    #
+    def build_max_heap(arr:, n:)
+      index = n / 2
 
-  # Right child of node at index "i" in binary heap
-  # @param [Integer] i
-  # @return [Integer]
-  #
-  def self.right(i:)
-    2 * i + 1
-  end
+      while index >= 1
+        arr = max_heapify_helper(arr:, i: index, heap_size: n)
+        index -= 1
+      end
 
-  # Parent of node at index "i" in binary heap
-  # @param [Integer] i
-  # @return [Integer]
-  #
-  def self.parent(i:)
-    i / 2
+      arr
+    end
+
+    # Insert a new key in max-heap
+    # @param [Integer] k
+    # @param [Array] arr
+    # @param [Integer] heap_size
+    # @return [Array]
+    #
+    def insert_key(k:, arr:, heap_size:)
+      arr_updated = prepare_arr_for_heap(arr:)
+      max_heap_arr = build_max_heap(arr: arr_updated, n: heap_size)
+
+      heap_size += 1
+      max_heap_arr[heap_size] = k
+
+      index = heap_size
+      max_heap_arr = float_node_up_until_root(index:, arr: max_heap_arr)
+
+      max_heap_arr[1..heap_size]
+    end
+
+    # Increase key of a node
+    # @param [Integer] i
+    # @param [Integer] new_key
+    # @param [Array] arr
+    # @param [Integer] heap_size
+    # @return [Array]
+    #
+    def increase_key(i:, new_key:, arr:, heap_size:)
+      arr[i] = new_key
+      arr_updated = float_node_up_until_root(index: i, arr:)
+
+      arr_updated[1..heap_size]
+    end
+
+    # Decrease key of a node
+    # @param [Integer] i
+    # @param [Integer] new_key
+    # @param [Array] arr
+    # @param [Integer] heap_size
+    # @return [Array]
+    #
+    def decrease_key(i:, new_key:, arr:, heap_size:)
+      arr[i] = new_key
+      arr_updated = max_heapify_helper(arr:, i:, heap_size:)
+
+      arr_updated[1..heap_size]
+    end
+
+    private
+
+    # Compare node with its parent until we find a parent that has value greater than or equal to node
+    # or we reach root
+    # @param [Integer] index
+    # @param [Array] arr
+    # @return [Array]
+    #
+    def float_node_up_until_root(index:, arr:)
+      while index > 1 && arr[parent(i: index)] < arr[index]
+        exchange_nodes(i: index, largest: parent(i: index), arr:)
+        index = parent(i: index)
+      end
+
+      arr
+    end
+
+    # Helper method to implement max_heapify
+    # @param [Array] arr
+    # @param [Integer] i
+    # @param [Integer] heap_size
+    # @return [Array]
+    #
+    def max_heapify_helper(arr:, i:, heap_size:)
+      left = left(i:)
+      right = right(i:)
+
+      # index "i" is a valid index within binary heap. However, left or right
+      # child of "i" maybe outside heap size
+      #
+      largest = if left <= heap_size && arr[left] > arr[i]
+                  left
+                else
+                  i
+                end
+
+      largest = right if right <= heap_size && arr[right] > arr[largest]
+
+      if largest != i
+        exchange_nodes(i:, largest:, arr:)
+        max_heapify_helper(arr:, i: largest, heap_size:)
+      end
+
+      # return Array arr which is a valid max heap
+      arr
+    end
+
+    # Prepares Arr, heap_size and returns an array which contains
+    # those values
+    # @param [Array] arr
+    # @param [Integer] n
+    # @return [Array]
+    #
+    def prepare_arr_for_heap(arr:)
+      arr_updated = [nil]
+      arr.each_with_index do |element, index|
+        arr_updated[index + 1] = element
+      end
+      arr_updated
+    end
+
+    # Helper method to implement heapsort
+    # @param [Array] arr
+    # @param [Integer] n
+    # @return [Array]
+    #
+    def heapsort_helper(arr:, n:)
+      exchange_nodes(i: 1, largest: n, arr:)
+      new_heap_size = n - 1
+      max_heapify_helper(arr:, i: 1, heap_size: new_heap_size)
+    end
+
+    # Exchanges nodes at index i with index_to_exchange
+    # @param [Integer] i
+    # @param [Array] arr
+    # @param [Integer] largest
+    #
+    def exchange_nodes(i:, largest:, arr:)
+      temp = arr[i]
+      arr[i] = arr[largest]
+      arr[largest] = temp
+    end
+
+    # Left child of node at index "i" in binary heap
+    # @param [Integer] i
+    # @return [Integer]
+    #
+    def left(i:)
+      2 * i
+    end
+
+    # Right child of node at index "i" in binary heap
+    # @param [Integer] i
+    # @return [Integer]
+    #
+    def right(i:)
+      2 * i + 1
+    end
+
+    # Parent of node at index "i" in binary heap
+    # @param [Integer] i
+    # @return [Integer]
+    #
+    def parent(i:)
+      i / 2
+    end
   end
 end
 
@@ -158,3 +224,18 @@ puts "Max Heapify :: #{MaxBinaryHeap.max_heapify(arr: arr1, i: 3, heap_size: arr
 
 arr2 = [2000, 1800, 1850, 200, 1600, 1500, 1800, 500, 600, 700, 650, 1000, 500, 1700, 100, -50, 75, 400, 500, 10, -110]
 puts "Max Heapify :: #{MaxBinaryHeap.max_heapify(arr: arr2, i: 4, heap_size: arr2.length + 1)}"
+
+arr3 = [2000, 1800, 1850, 1700, 1600, 1500, 1800, 500, 600, 700, 650]
+puts "Insert new element 4500 in #{arr3.inspect} - convert into max heap :: "
+print "\t \t \t #{MaxBinaryHeap.insert_key(k: 4500, arr: arr3, heap_size: arr3.length)}"
+puts
+
+arr4 = [nil, 4500, 1800, 2000, 1700, 1600, 1850, 1800, 500, 600, 700, 650, 1500]
+puts "Increase key of 1700 to 5000 at index 4 in #{arr4[1..arr4.length].inspect}"
+print "\t \t \t #{MaxBinaryHeap.increase_key(i: 4, new_key: 5000, arr: arr4, heap_size: arr4.length)}"
+puts
+
+arr5 = [nil, 4500, 1800, 2000, 1700, 1600, 1850, 1800, 500, 600, 700, 650, 1500]
+puts "Decrease key of 1800 to 400 at index 2 in #{arr5[1..arr5.length].inspect}"
+print "\t \t \t #{MaxBinaryHeap.decrease_key(i: 2, new_key: 400, arr: arr5, heap_size: arr5.length)}"
+puts
