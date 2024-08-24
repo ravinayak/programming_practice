215eb98995b7103179d069158bc7b9e9924c60bc
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-08-24 18:37:40 -0400

    Rubocop yml file changes, longest common subsequence implementation

--- grind_75/longest_common_subsequence.rb
+++ grind_75/longest_common_subsequence.rb
@@ -1,16 +1,18 @@
-# Given two strings text1 and text2, return the length of their longest common 
+# frozen_string_literal: true
+
+# Given two strings text1 and text2, return the length of their longest common
 # subsequence. If there is no common subsequence, return 0.
 
 # A subsequence of a string is a new string generated from the original string
-# with some characters (can be none) deleted without changing the relative order 
+# with some characters (can be none) deleted without changing the relative order
 # of the remaining characters.
 
 # For example, "ace" is a subsequence of "abcde".
-# A common subsequence of two strings is a subsequence that is common to both 
+# A common subsequence of two strings is a subsequence that is common to both
 # strings.
 
-# Time Complexity: 
-# O(m * n) => We have to iterate over all the chars of both words - word1, word2 
+# Time Complexity:
+# O(m * n) => We have to iterate over all the chars of both words - word1, word2
 # to find the longest common subesequence. word1.length = m, word2.length = n
 #
 # Space Complexity:
@@ -19,7 +21,7 @@
 # LCS (longest common subsequence)
 
 # Optimized Space Complexity:
-#  O(n) => We can use 2 rows each of size "n" to calculate LCS, where 
+#  O(n) => We can use 2 rows each of size "n" to calculate LCS, where
 # n = min(m,n)
 # Optimized Space Complexity is possible because of the fact that calculation of
 # LCS requires only 2 rows at any time
@@ -49,6 +51,155 @@
 #       options available to us
 #    d. We cannot choose lcs(i-2,j), or lcs(i,j-2) because this would mean we have
 #       to exclude 2 characters at one time from one of the strings
-#    e. We cannot choose lcs(i-1,j-1) either when 
+#    e. We cannot choose lcs(i-1,j-1) either when
 #         ith char of word1 != jth char of word2
 #       This would mean excluding 2 strings => 1 from each word, NOT ALLOWED
+
+# @param [String] word1
+# @param [String] word2
+# @return [Integer]
+#
+def lcs_space_optimized(word1:, word2:)
+  m = word1.length
+  n = word2.length
+
+  prev_row = Array.new(n + 1)
+  curr_row = Array.new(n + 1)
+
+  # prev_row => i-1
+  # curr_row => i
+  # For any iteration of i,j over words to find LCS,
+  #  prev_row[j] => Length of LCS for (i-1,j) i-1 chars of word1, j chars of word2
+  # curr_row[j-1] => Length of LCS for (i, j-1) i chars of word1, j-1 chars of word2
+  # We need these prev_row and one value in curr_row to calculate LCS
+
+  # To find LCS in space-optimized way, we would need to initialize prev_row
+  # when i=0, there cannot be any LCS for word1, word2 => Since word1 has i=0
+  #    => 0 chars of word1 => No subsequence possible
+  #    => prev_row[j] => 0 (for any j, when i = 0)
+
+  # when j=0, there cannot be any LCS for word1, word2 => Since word2 has j=0
+  #    => 0 chars of word2 => No subsequence possible
+  #    => curr_row[0] => 0 (for any i, when j = 0)
+
+  (0..n).each do |j|
+    prev_row[j] = 0
+  end
+
+  (1..m).each do |i|
+    # We need this in LCS calculation
+    curr_row[0] = 0
+    (1..n).each do |j|
+      if word1[i - 1] == word2[j - 1]
+        curr_row[j] = prev_row[j - 1] + 1 # prev_row[j-1] => (i-1, j-1)
+      else
+        # prev_row[j] 	=> (i-1, j)
+        # curr_row[j-1] => (i, j-1)
+        curr_row[j] = [prev_row[j], curr_row[j - 1]].max
+      end
+    end
+    # Duplicate the curr_row array to populate values in prev_row
+    # prev_row = curr_row => INCORRECT => Reference Assignment which will
+    # cause prev_row to change whenever curr_row is changed
+    prev_row = curr_row.dup
+  end
+
+  curr_row[n]
+end
+
+# @param [String] word1
+# @param [String] word2
+# @return [Integer]
+#
+def lcs(word1:, word2:)
+  m = word1.length
+  n = word2.length
+
+  # Here we do not have to separately initialize this 2D array to contain
+  # 0 values when m=0 for all n columns or when n=0 for all m rows
+  # The Array creation method here takes care of this for us, as we initialize
+  # the array to contain 0 as default value for all indices
+  lcs = Array.new(m + 1) { Array.new(n + 1, 0) }
+
+  (1..m).each do |i|
+    (1..n).each do |j|
+      if word1[i - 1] == word2[j - 1]
+        lcs[i][j] = lcs[i - 1][j - 1] + 1
+      else
+        lcs[i][j] = [lcs[i][j - 1], lcs[i - 1][j]].max
+      end
+    end
+  end
+
+  # To construct the actual longest common subsequence of word1 and word2, we
+  # backtrack from (m+1)(n+1)
+  # Logic is to follow the same algorithm we used when constructing LCS. Since
+  # lcs is a subsequence of two strings word1, word2 => A character will be
+  # included in lcs only when it is present in both word1 and word2
+  #    => word1[i] == word2[j] for any (i,j) => Only then we can include the
+  #       character in lcs
+  #    => In this use case, we move diagonally because in formula we selected
+  #       lcs[i-1][j-1] => "i" to "i-1", "j" to "j-1"
+  # if word1[i] != word2[j]
+  #    => According to the formula we used above, we have two directions:
+  #         a. going up in the row to previous row, i.e. from "i" to "i-1"
+  #         b. going left in the col to previous col, i.e. from "j" to "j-1"
+  #    => We can only change direction in one axis at one time
+  #    => In the formula above, we have choosen the maximum of possible values
+  #       which means, we should move in the direction
+  #         a. lcs[i][j-1] > lcs[i-1][j] => lcs[i][j-1] was choosen
+  #             => We move left of col "j" => i.e. from "j" to "j-1"
+  #         b. lcs[i][j-1] < lcs[i-1][j] => lcs[i-1][j] was choosen
+  #             => We move up above row "i" => i.e. from "i" to "i-1"
+  #    => We move in the direction of larger value
+
+  subsequence = []
+
+  i = m
+  j = n
+  while i.positive? && j.positive?
+    if word1[i - 1] == word2[j - 1]
+      subsequence << word1[i - 1]
+      # Move Diagonally
+      i -= 1
+      j -= 1
+    elsif lcs[i][j - 1] < lcs[i - 1][j]
+      # Move to previous row => i-1
+      i -= 1
+    else
+      # Here we must not use elsif lcs[i][j - 1] > lcs[i - 1][j]
+      # It is possible that lcs[i][j-1] = lcs[i-1][j], and in this
+      # use case, it will go into an infinite loop since i,j will
+      # continue to retain their values
+      # lcs[i][j - 1] >= lcs[i - 1][j] can be used, but best to
+      # use else, since no other use case is possible other than
+      # the above 3. If they are equal, we can move in any direction
+      # i,j => here we choose to move in col direction
+      # Move to left col => j-1
+      j -= 1
+    end
+  end
+
+  [lcs[m][n], subsequence.join('').reverse]
+end
+
+def test
+  subsequence_arr = [
+    { word1: 'abcde', word2: 'ace', lcs: 3, sub_lcs: 'ace' },
+    { word1: 'abc', word2: 'abc', lcs: 3, sub_lcs: 'abc' },
+    { word1: 'abc', word2: 'def', lcs: 0, sub_lcs: '' }
+  ]
+  subsequence_arr.each do |word_lcs_hsh|
+    word1 = word_lcs_hsh[:word1]
+    word2 = word_lcs_hsh[:word2]
+    lcs = word_lcs_hsh[:lcs]
+    sub_lcs = word_lcs_hsh[:sub_lcs]
+
+    puts "Word Inputs :: #{word1} -- #{word2}"
+    puts "Expected Output :: #{lcs} -- #{sub_lcs}"
+    print "Result of method call :: #{lcs(word1:, word2:)} \n"
+    puts "Result of Space Optimized :: #{lcs_space_optimized(word1:, word2:)}"
+  end
+end
+
+test
