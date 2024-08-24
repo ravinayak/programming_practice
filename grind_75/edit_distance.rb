032f70c55657ea433cb5954ed911d7f210c86f28
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-08-24 14:17:41 -0400

    Included optimized version of edit distance problem

--- grind_75/edit_distance.rb
+++ grind_75/edit_distance.rb
@@ -123,6 +123,10 @@
 #    before using the solution. This is not allowed, since only 1 transformation step is
 #    allowed
 
+# Time Complexity: O(m * n) => Since we have to iterate over both "m" and "n" chars of
+#                  word1 and word2
+# Space Complexity: O(m * n) => Since we need to store (m * n) entries in DP 2D array
+
 # Result:
 # The value at dp[m][n] will be the minimum edit distance between word1 and word2.
 
@@ -171,6 +175,102 @@ def edit_distance(word1:, word2:)
   dp[m][n]
 end
 
+# Time Complexity: O(m * n)
+# Space Complexity: O(n)
+#    We have been able to reduce space complexity because we only use
+#    2 1D arrays of size n+1 to store results of computation
+
+# @param [String] word1
+# @param [String] word2
+# @retrun [Integer]
+#
+def edit_distance_optimized(word1:, word2:)
+  m = word1.length
+  n = word2.length
+
+  # Initialize size to n+1, we will create 2 1D arrays with this
+  # size which will hold the minimum number of transformation
+  # operations required to convert "i" chars from word1 to "n"
+  # chars of word2
+  size = n + 1
+
+  # Initialize 2 arrays prev_row and curr_row with size
+  # This is based on the observation that during DP calculation
+  # we only 2 use rows at any time,
+  # dp[i][j] = dp[i-1][j-1] + 1 OR
+  # dp[i][j] = [ dp[i-1][j]], dp[i-1][j-1], dp[i][j-1] ].max + 1
+
+  # prev_row represents i-1
+  # curr_row represents i
+  # prev_row[j] represents the number of transformation operations
+  # required to convert "i-1" chars of word1 to "j" chars of word2
+  # for iteration value "i" of word1
+  # curr_row[j] for any iteration value "i" of word1 represents
+  # the minimum number of transformation operations required to
+  # convert "i" chars of word1 to "j" chars of word2
+
+  # curr_row for any iteration value "i" of word1 and "j" of word2
+  # represents the minimum number of transformation operations required
+  # to transform "i" chars of word1 into "j" chars of word2
+
+  # prev_row for any iteration value "i" of word1 and "j" of word2
+  # represents the minimum number of transformation operations required
+  # to transform "i-1" chars of word1 into "j" chars of word2
+
+  # Since we need prev_row to be initialized and one entry in curr_row
+  # (j-1) to calcualte dp[i][j], we have to take care of the base cases
+  # Iteration starts from 1 for "i", so prev_row must contain entries
+  # for i=0. prev_row for i = 0 and j = j, represents the minimum number
+  # of transformation operations required to convert "0" chars of word1
+  # to "j" chars of word2. This would require "j" char insertions, one
+  # after the other in sequence to obtain "j" chars of word2
+  # Also, curr_row[0] for any "i" value represents the minimum number
+  # of transformation operations required to convert  "i" chars of word1
+  # to "0" chars of word2. This would require "i" deletions from word1
+
+  # Create 2 arrays of prev_row and curr_row
+  prev_row = Array.new(size)
+  curr_row = Array.new(size)
+
+  # Initialize prev_row for base case: When i=0
+  (0..n).each do |j|
+    prev_row[j] = j
+  end
+
+  (1..m).each do |i|
+    # # i deletions required to convert "i" chars of word1 to "0" chars
+    # of word2. This is done outside the inner loop because inner loop
+    # starts from 1 and needs this value to evaluate DP algorithm
+    curr_row[0] = i
+    (1..n).each do |j|
+      if word1[i - 1] == word2[j - 1] # ith char of word1 = jth char of word2
+        # since ith char of word1 is same as jth char of word2, no operation
+        # is required when we have converted "i-1" chars of word1 to "j-1"
+        # chars of word2. "ith" char of word1 is already same as "jth" char
+        # of word2, hence we have "j" chars of word2
+        curr_row[j] = prev_row[j - 1]
+      else
+        curr_row[j] = [
+          prev_row[j] + 1, # [i-1, j] => 1 deletion from [i] => [i - 1]
+          # [i-1, j-1] => Substitute ith char of word1 with jth char of word2
+          prev_row[j - 1] + 1,
+          curr_row[j - 1] + 1 # [i, j-1] => 1 addition at the end result
+        ].min
+      end
+    end
+    # This is critical because for next iteration, we shall use the previously
+    # computed values for transformation operations
+    # prev_row = curr_row => This will assign prev_row a reference to curr_row
+    # which means any changes to curr_row will also affect curr_row making
+    # calculations incorrect in iteration
+    # When we use "dup", it creates a copy of curr_row array and assigns to
+    # prev_row
+    prev_row = curr_row.dup
+  end
+
+  curr_row[n]
+end
+
 def test
   test_arr = [
     { word1: 'kitten', word2: 'sitting', output: 3 },
@@ -183,8 +283,11 @@ def test
     word1 = word_output_hsh[:word1]
     word2 = word_output_hsh[:word2]
     output = word_output_hsh[:output]
+    edit_distance(word1:, word2:)
     res = edit_distance(word1:, word2:)
+    res1 = edit_distance_optimized(word1:, word2:)
     puts "Input words :: #{word1} -- #{word2}, Output :: #{res}, Expected Output :: #{output}"
+    puts "Input words :: #{word1} -- #{word2}, Output :: #{res1}, Expected Output :: #{output}"
   end
 end
 
