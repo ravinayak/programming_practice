d135f59fb6a4f1eef7e7148ee3db1da4ede86e94
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-09-22 06:38:43 -0400

    Images, Markdown for fb_messenger_whatsapp, rename screenshots to numbers updated

--- rename_screenshots_to_numbers.rb
+++ rename_screenshots_to_numbers.rb
@@ -5,11 +5,9 @@ require 'fileutils'
 # Define the directory where the screenshots are located
 # Replace 'screenshots_directory' with the path to your directory containing the screenshots
 directory_path = File.expand_path('~/Desktop/Videos')
-puts directory_path
 
 # Get all the screenshot files in the directory (assuming .png or .jpg for this example)
 screenshots = Dir.glob("#{directory_path}/*.{png,jpg,jpeg}")
-puts screenshots.inspect
 
 # Sort the screenshots based on the timestamps in their current filenames
 # Assuming timestamps are part of the filenames
@@ -19,7 +17,6 @@ sorted_screenshots = screenshots.sort_by { |file| File.mtime(file) }
 sorted_screenshots.each_with_index do |file, index|
   # Get the file extension (.png, .jpg, etc.)
   extension = File.extname(file)
-  puts extension
 
   # Define the new filename, starting with number 1, without padding (e.g., 1.png, 2.png)
   new_filename = "#{directory_path}/#{index + 1}#{extension}"
@@ -27,5 +24,5 @@ sorted_screenshots.each_with_index do |file, index|
   # Rename the file
   FileUtils.mv(file, new_filename)
 
-  puts "Renamed #{File.basename(file)} to #{File.basename(new_filename)}"
+  # puts "Renamed #{File.basename(file)} to #{File.basename(new_filename)}"
 end
---
+++ system_design/system_design_problem_solutions/markdown_files/fb_messenger_whatsapp.md
@@ -0,0 +1,55 @@
+Image - 001
+
+![design-solution-1](../images/fb_messenger_whatsapp/001.png)
+
+Image - 1
+
+![design-solution-1](../images/fb_messenger_whatsapp/1.png)
+
+Image - 2
+
+![design-solution-2](../images/fb_messenger_whatsapp/2.png)
+
+Image - 3
+
+![design-solution-3](../images/fb_messenger_whatsapp/3.png)
+
+Image - 4
+
+![design-solution-4](../images/fb_messenger_whatsapp/4.png)
+
+Image - 5
+
+![design-solution-5](../images/fb_messenger_whatsapp/5.png)
+
+Image - 6
+
+![design-solution-6](../images/fb_messenger_whatsapp/6.png)
+
+Image - 7
+
+![design-solution-7](../images/fb_messenger_whatsapp/7.png)
+
+Image - 8
+
+![design-solution-8](../images/fb_messenger_whatsapp/8.png)
+
+Image - 9
+
+![design-solution-9](../images/fb_messenger_whatsapp/9.png)
+
+Image - 10
+
+![design-solution-10](../images/fb_messenger_whatsapp/10.png)
+
+Image - 11
+
+![design-solution-11](../images/fb_messenger_whatsapp/11.png)
+
+Image - 12
+
+![design-solution-12](../images/fb_messenger_whatsapp/12.png)
+
+Image - 13
+
+![design-solution-13](../images/fb_messenger_whatsapp/001.png)
