e8c3b201f6a5c7039152e40791666b9de5f5ea09
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-09-24 07:27:23 -0400

    Improved implementation and documentation

--- grind_75/middle_linked_list.rb
+++ grind_75/middle_linked_list.rb
@@ -29,36 +29,49 @@ def middle_linked_list(head:)
   # If linked list is empty, return nil
   return nil if head.next.nil?
 
-  slow = fast = head.next
+  # This initialization is important because if we
+  # initialize slow = fast = head.next, we would have
+  # to check for fast being nil in iteration loop and
+  # only if fast is NOT NIL, we would advance slow pointer
+  # This is more work and more complex, than the simple
+  # condition of checking fast && fast.next, and letting
+  # the iteration loop stop whenever fast becomes NIL
+  # slow in this case will be the middle element
+  slow = fast = head
 
-  while fast&.next
-    # fast.next is not nil, so it is safe to access next
+  # This condition is logical because we are going to access
+  # a. fast = fast -> next -> next
+  #    => fast -> next: We can only access "next" pointer on fast
+  #       if fast != null
+  #    => fast -> next -> next: We can only access "next" pointer
+  #       on fast -> next if fast -> next is not null
+  while fast && fast.next # rubocop:disable Style/SafeNavigation
+    # fast is not null, fast.next is not nil, so it is safe to access next
     fast = fast.next.next
     # For even number of elements in list, fast will be
     # nil when slow is at the middle
 
     # Odd Number of nodes in linked list:
-    # Both slow/fast start at 1, fast moves 2 nodes at a
-    # time, hence fast will always land on an odd node
-    # (1 + Even) = odd => (odd + Even) = odd
+    # Both slow/fast start at 0, fast moves 2 nodes at a
+    # time, hence fast will always land on an even node
+    # (0 + Even) = Even
     # fast in such a case will reach the last node in the
     # linked list and slow will be at the exact middle.
     # fast.next = nil and hence iteration will stop
-    # num of nodes = 3, slow = 2, fast = 3 => iteration stops
-    # num of nodes = 5, slow = 3, fast = 5 => iteraiton stops
+    # num of nodes = 3, slow = 2, fast = nil => iteration stops
+    # num of nodes = 5, slow = 3, fast = nil => iteraiton stops
 
     # Even number of nodes in linked list:
-    # Both slow/fast start at 1, fast moves 2 nodes at a time,
+    # Both slow/fast start at 0, fast moves 2 nodes at a time,
     # hence fast will become nil in current iteration while
-    # slow is at the middle position. If we move slow, we will
-    # get incorrect result. Hence we must check if fast is still
-    # valid
+    # slow is at the middle position. The condition that fast != null
+    # will ensure that iteration stops, and slow will continue
+    # to retain its middle position, which we can return
     # num of nodes = 2, slow = 1, fast = 3 = nil in current
-    # iteration. If we move slow while fast is nil in current
-    # iteration, it will become 2 and be incorrect. Check for
-    # fast = nil before moving ahead
+    # iteration.
+    # In next iteration, fast != nil fails, and hence we stop
     # num of nodes = 4, slow = 2, fast = 5 = nil
-    slow = slow.next if fast
+    slow = slow.next
   end
 
   # slow points to the middle node
