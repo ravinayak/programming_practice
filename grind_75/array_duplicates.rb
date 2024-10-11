972e9a39d327bd5bd7cf2c726b5484481764020e
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-10-11 18:53:41 -0400

    Array duplicate, array duplicates solution using hash/sorting methods

--- grind_75/array_duplicates.rb
+++ grind_75/array_duplicates.rb
@@ -10,7 +10,10 @@
 # @return [Ineger]
 #
 def find_duplicate(input_arr:)
-  return nil if input_arr.empty? || input_arr.length == 1
+  if input_arr.empty? || input_arr.length == 1
+    print "\n 1st Duplicate :: NIL"
+    return
+  end
 
   count_hsh = {}
   duplicate = nil
@@ -25,13 +28,49 @@ def find_duplicate(input_arr:)
   end
 
   # Return duplicate
+  print "\n 1st Duplicate :: #{duplicate.nil? ? 'NIL' : duplicate}"
   duplicate
 end
 
-arr = [[1, 2, 1, 2, 3, 4, 5, 5, 6, 5], [1, 2, 3], [], [1]]
+def find_duplicates_using_hsh(arr:)
+  count_hsh = {}
+  duplicates = Set.new
+
+  arr.each do |element|
+    if count_hsh.key?(element)
+      count_hsh[element] += 1
+    else
+      count_hsh[element] = 1
+    end
+  end
+
+  arr.each do |element|
+    duplicates.add(element) if !duplicates.include?(element) && count_hsh[element] > 1
+  end
+
+  print "\n Duplicates using Hash :: #{duplicates.to_a.inspect}"
+  duplicates.to_a
+end
+
+def find_duplicates_using_sort(arr:)
+  arr_sorted = arr.sort
+  duplicates = Set.new
+  (1...arr.length).each do |index|
+    if !duplicates.include?(arr_sorted[index]) && arr_sorted[index] == arr_sorted[index - 1]
+      duplicates.add(arr_sorted[index])
+    end
+  end
+
+  print "\n Duplicates using Sort :: #{duplicates.to_a.inspect}"
+  duplicates.to_a
+end
+
+arr = [[1, 2, 1, 2, 3, 4, 5, 5, 6, 5], [1, 2, 3], [], [1], [1, 2, 3, 4, 5, 1, 90, 87, 4]]
 
 arr.each do |input_arr|
-  duplicate = find_duplicate(input_arr:)
-  duplicate = 'NIL' if duplicate.nil?
-  puts "Input Arr :: #{input_arr.inspect}, duplicate :: #{duplicate}"
+  print "\n Input Arr :: #{input_arr.inspect}\n"
+  find_duplicate(input_arr:)
+  find_duplicates_using_sort(arr: input_arr)
+  find_duplicates_using_hsh(arr: input_arr)
+  print "\n\n"
 end
