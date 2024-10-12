58af4fea2648b0d4a6da2cd18370e16be6348652
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-10-12 18:12:24 -0400

    Included bottom up approach for inverting binary tree, included more documentation for non-recursive approach that uses stack

--- grind_75/invert_binary_tree.rb
+++ grind_75/invert_binary_tree.rb
@@ -16,6 +16,8 @@ def invert_binary_tree(root:)
   root
 end
 
+# This is a Top-Down Approach to inverting the binary tree where
+# we swap left/right nodes at the top
 # @param [Node] node
 #
 def invert_binary_tree_util(node:)
@@ -29,6 +31,21 @@ def invert_binary_tree_util(node:)
 
   invert_binary_tree_util(node: node.left)
   invert_binary_tree_util(node: node.right)
+end
+
+# This is a bottom up approach where we swap the nodes when we reach
+# the bottom of the tree, i.e. leaf nodes and all the way up in the
+# tree
+def invert_bt_util(node:)
+  return nil if node.nil?
+
+  node_left = invert_bt_util(node: node.left)
+  node_right = invert_bt_util(node: node.right)
+
+  node.right = node_left
+  node.left = node_right
+
+  node
 end
 
 # @param [Node] node
@@ -48,6 +65,12 @@ def invert_binary_tree_util_non_rec(node:)
 
     node.left, node.right = node.right, node.left
 
+    # Order of pushing nodes onto stack does not matter. This is because
+    # goal of using stack is to pop the node and invert its left/right
+    # subtree, whether we accomplish this for left node or right node
+    # 1st does not really matter
+    # The other method which is a bottom up approach will be harder to
+    # implement using Stack, top-down approach is simpler to implement
     st.push(data: node.right)
     st.push(data: node.left)
   end
