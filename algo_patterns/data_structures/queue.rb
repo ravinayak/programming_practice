bf1108b3def2fa622e88257d74064491e431ca59
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-11-12 22:38:29 -0500

    Included implementation of queue using arr, rear, front

--- algo_patterns/data_structures/queue.rb
+++ algo_patterns/data_structures/queue.rb
@@ -1,76 +1,93 @@
-# frozen_string_literal: true
-
-# Implement Queue class in Ruby
-#
+# Queue class implementation using arr, rear, front
 class Queue
-  attr_accessor :queue
+  attr_accessor :arr, :rear, :front
 
+  # Front <=> Index in stack for display and peek
+  # Rear <=> Index for initialization
   def initialize
-    @queue = []
+    @arr = []
+    @rear = -1
+    @front = 0
   end
 
-  # Add element to queue
-  # @param [Integer] data
-  # @return [Integer]
-  #
-  def enqueue(data:)
-    queue.push(data)
-  end
-
-  # Is Queue empty
-  # @return [Boolean]
-  #
   def empty?
-    return true if queue.empty?
-
-    false
+    rear < front
   end
 
-  # Remove element from queue
-  # @return [Integer]
-  #
+  def enqueue(data:)
+    @rear += 1
+    @arr[rear] = data
+  end
+
   def dequeue
-    if queue.empty?
-      puts 'Queue is empty'
-      nil
-    else
-      queue.shift
+    return nil if empty?
+
+    element = element_at_front
+    @front += 1
+    element
+  end
+
+  def size
+    # When empty? rear < front and rear - front + 1
+    # will give correct answer, yet for Precautionary
+    # measure, we return
+    # Consider, rear = -1, front = 0 (Queue is empty)
+    # rear - front + 1 = -1 -0 + 1 = 0
+    #
+    return 0 if empty?
+
+    # Rear points to element which has been added to queue
+    # front points to element which has still not been deleted,
+    # every element b4 front has been deleted
+    # rear - front = <front + 1, front + 2, front + 3,..., rear>
+    # Add 1 to include element at front position
+    rear - front + 1
+  end
+
+  def display
+    return if empty?
+
+    stack_front = front
+    while stack_front <= rear
+      print "#{arr[stack_front]} "
+      stack_front += 1
     end
   end
 
-  # Peek method - Returns the 1st element in queue without removing it
-  # @return [Integer|nil]
-  #
+  # Peek implies element at front of Queue, Queue is FIFO - First in First Out
+  # First in => Front
+  # Last in => Rear
+  # Front <=> Index in stack for display and peek
   def peek
-    return nil if queue.empty?
-
-    queue.first
+    element_at_front
   end
 
-  # Returns size of queue
-  # @return [Integer]
-  #
-  def size
-    queue.length
-  end
-
-  # Display elements in queue
-  #
-  def display
-    puts "Queue :: #{queue.inspect}"
+  def element_at_front
+    arr[front]
   end
 end
 
-# queue = Queue.new
-# queue.enqueue(data: 5)
-# queue.enqueue(data: 6)
-# queue.enqueue(data: 7)
-# queue.display
-# puts queue.peek
-# puts queue.dequeue
-# puts queue.size
-# puts queue.empty?
-# puts queue.dequeue
-# queue.display
-# puts queue.dequeue
-# queue.display
+def test
+  queue = Queue.new
+  puts " Queue Empty :: #{queue.empty?}"
+  puts " Queue Size :: #{queue.size}"
+  queue.enqueue(data: 5)
+  queue.enqueue(data: 6)
+  queue.enqueue(data: 7)
+  print ' Queue Elements being displayed :: '
+  queue.display
+  print "\n"
+  puts " Queue Peek :: #{queue.peek}"
+  puts " Queue Dequeued :: #{queue.dequeue}"
+  puts " Queue Size :: #{queue.size}"
+  puts " Queue Empty :: #{queue.empty?}"
+  puts " Queue Dequeued :: #{queue.dequeue}"
+  print ' Queue Elements being displayed :: '
+  queue.display
+  puts
+  puts " Queue Dequeued :: #{queue.dequeue}"
+  puts " Queue Empty :: #{queue.empty?}"
+  puts " Queue Elements being displayed :: #{queue.display}"
+end
+
+test
