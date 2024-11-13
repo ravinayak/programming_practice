2ec14d379999a5c08352b285103b75c225c6136e
Ravi Kumar Nayak <ravinayak19@gmail.com>
2024-11-13 18:22:01 -0500

    Simplified List impelementation and node class to allow data to be nil in initialization

--- algo_patterns/data_structures/list.rb
+++ algo_patterns/data_structures/list.rb
@@ -7,125 +7,120 @@ require_relative 'linked_list'
 class List
   # Attribute Accessor
   #
-  attr_accessor :input_arr, :head, :list
+  attr_accessor :head
 
-  # Initialize method
-  #
-  def initialize(input_arr:)
-    @input_arr = input_arr
-    @head, @list = *prepare_list
+  def initialize
+    @head = Node.new
   end
 
-  # Prepares linked list for given input array elements
-  # @return [Node] 1st Node of linked list
-  #
-  def prepare_list
-    list = []
-    curr_index = 0
-    # Initialize a header node with nil data. This will act
-    # as the dummy node with which we can start iteration
-    # of linked list
-    node_previous = Node.new(data: nil)
-    # head node is not put into the array because it does not
-    # contain any element. A list maps elements in array to
-    # nodes in linked list. head does not contain any data
-    # and is only a placeholder to reference the 1st node in
-    # linked list. So, the next line is commented out
-    # list << node_previous
-    head = node_previous
-    while curr_index < input_arr.length
-      node_previous, curr_index = *prepare_list_helper(input_arr:, curr_index:, list:,
-                                                       node_previous:)
+  def insert(data:)
+    prev = head
+    curr = head.next
+
+    while curr
+      prev = curr
+      curr = curr.next
     end
-    [head, list]
+    prev.next = Node.new(data:)
+    head
   end
 
-  # Helper method to prepare List
-  # @param [Array] input_arr
-  # @param [Integer] curr_index
-  # @param [Array] list
-  # @param [Node] node_previous
-  # @return [Array]
-  #
-  def prepare_list_helper(input_arr:, curr_index:, list:, node_previous:)
-    node_next = Node.new(data: input_arr[curr_index])
-    list << node_next
-    node_previous.next = node_next
-    node_previous = node_next
-    [node_next, curr_index + 1]
-  end
+  def remove(data:)
+    return if empty?
 
-  # Insert an element at index in the list
-  # @param [Integer] index
-  # @return [List]
-  #
-  def insert_element(element:, index:)
-    return insert_element_at_head(element:) if index.zero?
+    prev = head
+    curr = head.next
 
-    insert_element_at_non_head(element:, index:)
-  end
-
-  # Insert element when index is 0
-  # @param [Node] node
-  #
-  def insert_element_at_head(element:)
-    new_node = Node.new(data: element)
-    # Reference to current 1st node in list
-    temp = head.next
-    # Update the head to point to new node as the 1st node
-    head.next = new_node
-    # Current node should point to previous 1st node as
-    # next
-    new_node.next = temp
-    # Prepend new node at the beginnning of the list
-    list.unshift(new_node)
-  end
-
-  # Insert element when index is > 0
-  # @param [Integer] element
-  # @param [Integer] index
-  #
-  def insert_element_at_non_head(element:, index:)
-    new_node = Node.new(data: element)
-    # Node at index - 1 should point to new node which would be
-    # inserted at index position in the list. This new node
-    # should point to the node currently at index position in
-    # the list
-    node_prev = list[index - 1]
-    node_prev.next = new_node
-    new_node.next = list[index]
-    # All elements starting from and including index position
-    # should be shifted by 1 position in the list
-    shift_nodes_in_list(index:)
-    list[index] = new_node
-  end
-
-  # Prepares a new list which has element inserted at non_head index
-  # @param [Integer] index
-  # @return [NIL]
-  #
-  def shift_nodes_in_list(index:)
-    # Idea here is to start from the end of list and copy the
-    # elements to one position greater than their current index
-    # This will shift all the elements from the end of list to
-    # the index position where we want to insert new element to
-    # one position higher. Thus index position becomes free to
-    # include new node
-    iteration = list.length - 1
-    while iteration >= index
-      list[iteration + 1] = list[iteration]
-      iteration -= 1
+    while curr
+      if curr.data == data
+        prev.next = curr.next
+        curr.next = nil
+        return curr.data
+      end
+      prev = curr
+      curr = curr.next
     end
+    head
+  end
+
+  def search(data:)
+    return if empty?
+
+    curr = head.next
+    while curr
+      return curr.data if curr.data == data
+
+      curr = curr.next
+    end
+    nil
+  end
+
+  def empty?
+    head.next.nil?
+  end
+
+  # Both insert_data and remove_data have the exact same structure
+  def insert_data(data:, index:)
+    # To insert, start count at 1, because we can insert at index 1 even if
+    # the element does not exist at that index, say when list is empty, we
+    # can insert data at index 1
+    count = 1
+    prev = head
+    curr = head.next
+
+    while curr && count < index
+      prev = curr
+      curr = curr.next
+      count += 1
+    end
+
+    return 'Element cannot be inserted at specified index' if count < index
+
+    node = Node.new(data:)
+    prev.next = node
+    node.next = curr
+
+    head
+  end
+
+  def remove_data(index:)
+    # This ensures that there is a node to remove, and we can safely initialize
+    # count to 1 because when count = 1, curr is the 1st node in list and no
+    # iteration of while loop is run
+    # For subsequent runs, we increment count and move curr to the point to that
+    # specific node
+    return if empty?
+
+    count = 1
+    prev = head
+    curr = head.next
+
+    while curr && count < index
+      count += 1
+      prev = curr
+      curr = curr.next
+    end
+
+    return 'Element cannot be removed from specified index' if count < index
+
+    prev.next = curr.next
+    curr.next = nil
+
+    head
   end
 
   # Traverse List
   # @return [NIL]
   #
   def traverse_list
+    return 'Empty List' if empty?
+
     # LinkedList.traverse_list(self.head)
     puts '**************************** List ****************************'
-    list.each do |node|
-      print " #{node.data}  "
+    curr = head.next
+    while curr
+      print " #{curr.data} "
+      curr = curr.next
     end
     puts "\n*************************************************************"
   end
@@ -133,14 +128,21 @@ end
 
 def test
   input_arr = [1, 3, 5, 6, 7, 8]
-  puts "Given Input Array :: #{input_arr}"
-  list1 = List.new(input_arr:)
+  list1 = List.new
+  input_arr.each do |element|
+    list1.insert(data: element)
+  end
   list1.traverse_list
-  list1.insert_element(element: 12, index: 0)
-  list1.insert_element(element: 15, index: 5)
-  print "\n List with element 12 inserted at 0th index, and element 15 "
+  list1.insert_data(data: 12, index: 1)
+  list1.insert_data(data: 15, index: 5)
+  print "\n List with element 12 inserted at 1st index, and element 15 at index 5"
   puts 'inserted at 5th index in the above list is as below :: '
+  list1.traverse_list
+  list1.remove_data(index: 5)
+  list1.remove_data(index: 6)
+  list1.remove_data(index: 1)
+  print "\n List with elements at index 5 and then at index 1 removed :: Should remove elements 15, 7 and 12 \n"
   list1.traverse_list
 end
 
-# test
+test
--- algo_patterns/data_structures/node.rb
+++ algo_patterns/data_structures/node.rb
@@ -5,7 +5,7 @@
 class Node
   attr_accessor :data, :next
 
-  def initialize(data:, next_node: nil)
+  def initialize(data: nil, next_node: nil)
     @data = data
     @next = next_node
   end
